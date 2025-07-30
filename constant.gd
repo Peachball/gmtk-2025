d098ec5e0909a5d69ff397a859a4f76086f31d27
Peachball <chenyexu@gmail.com>
2025-07-30 12:39:28 -0700

    Update tile

--- constant.gd
+++ constant.gd
@@ -3,3 +3,7 @@ extends Node
 class_name Constant
 
 const TILE_WIDTH := 64
+
+enum Direction { 
+	UP, RIGHT, DOWN, LEFT
+}
---
+++ objects/tile.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+class_name Tile
+
+# Takes in integers that are represented by Constant.DIRECTION
+func _init(edge1: int, edge2: int) -> void:
+	pass
