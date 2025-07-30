10845bacc1c85b5a31d5d42ccca564ec3f73a30c
Max Haak <haakm7@gmail.com>
2025-07-30 13:39:53 -0700

    tiles and grid of tiles

---
+++ objects/slot_machine.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+class_name SlotMachine
+
+const MACHINE_WIDTH := 3
+const MACHINE_HEIGHT := 3
+
+@export var tile_scene: PackedScene
+@export var tile_spacing := 5
+	
+func _ready() -> void:
+	spawn_tiles()
+	
+func spawn_tiles() -> void:
+	for row in MACHINE_HEIGHT:
+		for col in MACHINE_WIDTH:
+			var tile := tile_scene.instantiate()
+			
+			var tile_size := Constant.TILE_WIDTH
+			var pos_x := col * (tile_size + tile_spacing)
+			var pos_y := row * (tile_size + tile_spacing)
+			
+			tile.position = Vector2(pos_x, pos_y)
+			add_child(tile)
--- objects/tile.gd
+++ objects/tile.gd
@@ -2,6 +2,48 @@ extends Node2D
 
 class_name Tile
 
-# Takes in integers that are represented by Constant.DIRECTION
-func _init(edge1: int, edge2: int) -> void:
-	pass
+var end := 0
+var line_thickness := 3
+var inset := 2
+
+func _ready() -> void:
+	end = randi() % 2
+
+func _draw() -> void:
+	# draw box
+	var rect_position = Vector2(0, 0)
+	var rect_size = Vector2(Constant.TILE_WIDTH, Constant.TILE_WIDTH)
+	var my_rect = Rect2(rect_position, rect_size)
+	
+	var black = Color(0, 0, 0)
+	var white = Color(1, 1, 1)
+	
+	draw_rect(my_rect, white , true)
+	
+	# Adjust position and size to account for border thickness
+	var border_rect_position = rect_position - Vector2(line_thickness, line_thickness)
+	var border_rect_size = rect_size + Vector2(line_thickness * 2, line_thickness * 2)
+	draw_rect(Rect2(border_rect_position, border_rect_size), black, false, line_thickness)
+
+	# draw path
+	draw_line(
+		Vector2(inset, Constant.TILE_WIDTH / 2),
+		Vector2(Constant.TILE_WIDTH / 2, Constant.TILE_WIDTH / 2),
+		black,
+		line_thickness
+	)
+	if end == 0:
+		draw_line(
+			Vector2(Constant.TILE_WIDTH / 2, Constant.TILE_WIDTH / 2),
+			Vector2(Constant.TILE_WIDTH - inset, Constant.TILE_WIDTH / 2),
+			black,
+			line_thickness
+		)
+	else:
+		draw_line(
+			Vector2(Constant.TILE_WIDTH / 2, Constant.TILE_WIDTH / 2),
+			Vector2(Constant.TILE_WIDTH / 2, Constant.TILE_WIDTH - inset),
+			black,
+			line_thickness
+		)
+	
